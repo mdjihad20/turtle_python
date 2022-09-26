28ca8d1264457a60c97f2a06b69a6a515224b895
mdjihad20 <112076679+mdjihad20@users.noreply.github.com>
2022-09-27 03:15:51 +0600

    Add files via upload

---
+++ square_animation_turtle.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+pensize(10)
+color('yellow')
+bgcolor('black')
+speed(1)
+
+penup()
+goto(-150, 150)
+fillcolor('red')
+begin_fill()
+pendown()
+
+forward(300)
+left(-90)
+forward(300)
+left(-90)
+forward(300)
+left(-90)
+forward(300)
+left(-90)
+
+end_fill()
